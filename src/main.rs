d0c010aec7df0b6c0596cbdab52b470e1984a236
zhouzhangqi <1978892854@qq.com>
2024-04-19 23:29:40 +0800

    fitst class

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+use rcli::process_csv;
+use rcli::{Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    Ok(())
 }
