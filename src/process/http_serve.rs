ae18bd774becadff3e3f8fcd78178aa916d59629
zhouzhangqi <1978892854@qq.com>
2024-04-24 20:21:19 +0800

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -6,6 +6,7 @@ use axum::{
     routing::get,
     Router,
 };
+use tower_http::services::fs::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -16,10 +17,11 @@ struct HttpServeState {
 pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on port {}", path, addr);
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
 
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", ServeDir::new(path))
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -39,6 +41,9 @@ async fn file_handler(
             format!("File {} not found", p.display()),
         )
     } else {
+        // if it is a directory list all files/subdirectories
+        // as <li><a href="/path/to/file">file</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
@@ -52,5 +57,21 @@ async fn file_handler(
                 )
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let path = Path("Cargo.toml".to_string());
+        let (status, content) = file_handler(State(state), path).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
